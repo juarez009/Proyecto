0b88d5986758edff36773f33145783f33bcfb859
Antzng <136501735+Antzng@users.noreply.github.com>
2023-09-14 10:55:35 -0600

    Add files via upload
    actualización de la tabla MySQL

---
+++ blockchains.sql
@@ -0,0 +1,194 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 14-09-2023 a las 18:52:49
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `blockchains`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalle_transaccion`
+--
+
+CREATE TABLE `detalle_transaccion` (
+  `id_detalle` int(11) NOT NULL,
+  `id_transaccion` int(11) NOT NULL,
+  `id_producto` int(20) NOT NULL,
+  `cantidad` int(20) NOT NULL,
+  `precio_unitario` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE `producto` (
+  `id` int(20) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `descripcion` text NOT NULL,
+  `precio` decimal(10,2) NOT NULL,
+  `stock` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `producto`
+--
+
+INSERT INTO `producto` (`id`, `nombre`, `descripcion`, `precio`, `stock`) VALUES
+(1, 'Cuadernos grapados', 'Cuadernos de rayas engrapados.', 0.65, 21),
+(2, 'Lapiceros (rojo, negro y azul)', 'Lapiceros bic de diferentes colores.', 0.25, 45),
+(3, 'Cosedora', 'Maquina para coser.', 10.65, 10),
+(4, 'Ganchos para cosedoras', 'ganchos de repuesto para cosedora.', 2.50, 50),
+(5, 'Block iris', 'Block de colores.', 1.50, 15),
+(6, 'Folder tamaño carta', 'Folder de colores tamaño carta.', 0.25, 10),
+(7, 'Recibo de caja menor', 'Recibos comerciales.', 0.25, 25),
+(8, 'Lapiz N2', 'Lapiz para dibujar N2', 0.45, 50),
+(9, 'Caja de colores', 'Caja de 24 colores.', 2.50, 22),
+(10, 'Bisturi', 'bisturi para cortar diferentes tipos de papel o carton.', 3.15, 19),
+(11, 'Cinta de enmascarar', 'Cinta para enmascarar.', 0.75, 21),
+(12, 'Cuadernos argollados', 'Cuadernos de rayas argollados.', 1.00, 22),
+(13, 'Block rayado carta y oficio', 'Block de notas tamaño carta y tamaño oficio.', 0.40, 41),
+(14, 'Grapadora de uno o dos huecos', 'Grapadora mediana.', 2.50, 17),
+(15, 'Tablero de corcho', 'Tablero de corte.', 6.35, 11),
+(16, 'Marcadores borrables para tablero', 'Plumones para pizarra.', 1.25, 20),
+(17, 'Tizas para tablero', 'Tiza para pizarra.', 0.75, 17),
+(18, 'Borrados para marcadores', 'Borrador para pizarra.', 3.00, 19),
+(19, 'Papel carta para impresora', 'Papel bond color blanco.', 0.01, 5),
+(20, 'Block de dibujo', 'Block en blanco tamaño carta y tamaño oficio.', 0.75, 15),
+(21, 'Bitacoras', 'Bitacoras de anotaciones.', 1.25, 23),
+(22, 'Marcadores de vinilo', 'Marcadores.', 1.50, 13),
+(23, 'Micropuntas de colores', 'Micro puntas de respuesto.', 2.00, 13),
+(24, 'Portaminas', 'Porta minas de repuesto.', 1.60, 15),
+(25, 'Lapiz puntaminas', 'Lapiz puntamina sencillo', 1.50, 12),
+(40, 'Papel', 'nose', 7.00, 14),
+(44, 'Libreta', 'para dibujar', 2.00, 12);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `transaccion`
+--
+
+CREATE TABLE `transaccion` (
+  `id` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `fecha` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `correo` varchar(255) NOT NULL,
+  `codigo` int(6) NOT NULL,
+  `rol` enum('administrador','usuario') NOT NULL DEFAULT 'usuario',
+  `contrasenia_hash` varchar(60) NOT NULL,
+  `imagen` blob DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nombre`, `correo`, `codigo`, `rol`, `contrasenia_hash`, `imagen`) VALUES
+(1, 'Jimy', 'js@gmail.com', 0, 'administrador', 'xaxa', NULL),
+(2, 'Matias', 's@gmail.com', 123456, 'usuario', '$2b$10$.h/l8QJ0Xg9/m9QWPRmPEeq6235wIspYxJMpqqMbD.Abcxukk2q/6', NULL);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `detalle_transaccion`
+--
+ALTER TABLE `detalle_transaccion`
+  ADD PRIMARY KEY (`id_detalle`),
+  ADD KEY `id_transaccion` (`id_transaccion`),
+  ADD KEY `id_producto` (`id_producto`);
+
+--
+-- Indices de la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `transaccion`
+--
+ALTER TABLE `transaccion`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_usuario` (`id_usuario`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `detalle_transaccion`
+--
+ALTER TABLE `detalle_transaccion`
+  MODIFY `id_detalle` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `producto`
+--
+ALTER TABLE `producto`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+
+--
+-- AUTO_INCREMENT de la tabla `transaccion`
+--
+ALTER TABLE `transaccion`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `detalle_transaccion`
+--
+ALTER TABLE `detalle_transaccion`
+  ADD CONSTRAINT `detalle_transaccion_ibfk_1` FOREIGN KEY (`id_transaccion`) REFERENCES `transaccion` (`id`),
+  ADD CONSTRAINT `detalle_transaccion_ibfk_2` FOREIGN KEY (`id_producto`) REFERENCES `producto` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
