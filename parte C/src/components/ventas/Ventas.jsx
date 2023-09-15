4c318ce0de3ea078f0d16e4923871713d52e1f87
Chris-Medrano <144301394+Chris-Medrano@users.noreply.github.com>
2023-09-15 17:43:11 -0600

    Update Ventas.jsx
    Importar el componente Cards

--- parte C/src/components/ventas/Ventas.jsx
+++ parte C/src/components/ventas/Ventas.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import './Ventas.css'
 import { Link } from "react-router-dom";
+import Cards from "./Cards";
 //import QRCode from "react-qr-code";
 
 const Ventas = () => {
